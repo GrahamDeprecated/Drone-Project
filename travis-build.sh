72e82ee0e2784df132e0d782e21bbf01d4d501a8
Peter Lotts <plpub2@gmail.com>
2013-05-21 19:47:40 +0100

    travis testing + remove absolute paths

--- travis-build.sh
+++ travis-build.sh
@@ -3,11 +3,15 @@
 #arg=filename
 function up_file()
 {
-	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=$1&file=`cat $1 | base64 --wrap 0`" http://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
+	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=$1&file=`cat $1 | base64 --wrap 0`" http://pml369-builds.suroot.com/UPLOAD.php >> /dev/null
+	ret=$?
+	if [ ! $ret == 0]; then echo "Upload $1: wget returned $ret"; fi
 }
 function up_fin()
 {
-	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=.completed&file=`date | base64 --wrap 0`" http://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
+	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=.completed&file=`date | base64 --wrap 0`" http://pml369-builds.suroot.com/UPLOAD.php >> /dev/null
+	ret=$?
+	if [ ! $ret == 0]; then echo "Upload $1: wget returned $ret"; fi
 }
 
 up_file drone_proj.vcxproj
