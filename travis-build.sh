43ba942f2964eefdbd8000fbd1fd6667504bc75c
Peter Lotts <plpub2@gmail.com>
2013-05-21 16:42:05 +0100

    travis testing

--- travis-build.sh
+++ travis-build.sh
@@ -3,11 +3,11 @@
 #arg=filename
 function up_file()
 {
-	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=$1&file=`cat $1`" http://pml369-builds.suroot.com/UPLOAD.php
+	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=$1&file=`cat $1 | base64 --wrap 0`" http://pml369-builds.suroot.com/UPLOAD.php >> /dev/null
 }
 function up_fin()
 {
-	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=.completed&file=`date`" http://pml369-builds.suroot.com/UPLOAD.php
+	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=.completed&file=`date | base64 --wrap 0`" http://pml369-builds.suroot.com/UPLOAD.php >> /dev/null
 }
 
 up_file drone_proj.vcxproj
