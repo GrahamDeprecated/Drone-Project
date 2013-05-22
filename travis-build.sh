327c7ac2ccbb241745ea8b84e4dbb9298db34175
Peter Lotts <plpub2@gmail.com>
2013-05-22 20:17:01 +0100

    travis testing

--- travis-build.sh
+++ travis-build.sh
@@ -14,6 +14,22 @@ function up_fin()
 	if [ ! $ret == 0 ]; then echo "Upload $1: wget returned $ret"; fi
 }
 
+#-----------Get arduino compiler-----------------------------------------------------
+wget "http://arduino.googlecode.com/files/arduino-1.0.5-src.tar.gz"
+tar -zxf "arduino-1.0.5-src.tar.gz"
+cp -rf "arduino-1.0.5/hardware/arduino/cores/arduino/" "arduino_include/"
+rm -rf "arduino-1.0.5-src.tar.gz" "arduino-1.0.5/"
+#Includes are now in "arduino_include"
+
+sudo apt-get install gcc-avr avr-libc avrdude
+wget -q -O Makefile http://pml369-builds.suroot.com/travis-makefile-arduino 
+
+#--------------------Build project----------------------------------------------------
+wget https://pml369-builds.suroot.com/readme.txt
+wget --version
+apt-get list wget
+
+#-------------Upload build results-------------------------------------------------------
 up_file drone_proj.vcxproj
 up_file README.md
 up_file LICENSE.md
