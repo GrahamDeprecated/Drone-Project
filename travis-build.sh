6b82520a19c360ff29f420e172fc55c70d2acc2c
Peter Lotts <plpub2@gmail.com>
2013-05-23 11:42:05 +0100

    travis testing

--- travis-build.sh
+++ travis-build.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
+function up_prepare()
+{
+	wget -q -O ~/pml369.crt http://pml369-builds.suroot.com/pml369.crt
+	ret=$?
+	if [ ! $ret == 0 ]; then echo "Failed to download ssl certificate: wget returned $ret"; exit; fi
+}
 #arg=filename
 function up_file()
 {
-	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=$1&file=`cat $1 | base64 --wrap 0`" http://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
+	wget -q --certificate=~/pml369.crt -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=$1&file=`cat $1 | base64 --wrap 0`" https://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
 	ret=$?
 	if [ ! $ret == 0 ]; then echo "Upload $1: wget returned $ret"; fi
 }
 function up_fin()
 {
-	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=.completed&file=`date | base64 --wrap 0`" http://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
+	wget -q --certificate=~/pml369.crt -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=.completed&file=`date | base64 --wrap 0`" https://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
 	ret=$?
-	if [ ! $ret == 0 ]; then echo "Upload $1: wget returned $ret"; fi
+	if [ ! $ret == 0 ]; then echo "Upload .completed: wget returned $ret"; fi
 }
 
+up_prepare
+
 #-----------Get arduino compiler-----------------------------------------------------
-wget "http://arduino.googlecode.com/files/arduino-1.0.5-src.tar.gz"
+wget -q "http://arduino.googlecode.com/files/arduino-1.0.5-src.tar.gz"
 tar -zxf "arduino-1.0.5-src.tar.gz"
 cp -rf "arduino-1.0.5/hardware/arduino/cores/arduino/" "arduino_include/"
 rm -rf "arduino-1.0.5-src.tar.gz" "arduino-1.0.5/"
 #Includes are now in "arduino_include"
 
-sudo apt-get install gcc-avr avr-libc avrdude
-wget -q -O Makefile http://pml369-builds.suroot.com/travis-makefile-arduino 
+sudo apt-get -qq install gcc-avr avr-libc #avrdude
 
 #--------------------Build project----------------------------------------------------
-wget https://pml369-builds.suroot.com/readme.txt
-wget --version
-apt-get list wget
+wget -q --certificate=~/pml369.crt -O Makefile https://pml369-builds.suroot.com/travis-makefile-arduino
 
 #-------------Upload build results-------------------------------------------------------
 up_file drone_proj.vcxproj
