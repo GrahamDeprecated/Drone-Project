5a1dafed4f4f6b5f0a3052d8e2e828b876f0a0d8
Peter Lotts <plpub2@gmail.com>
2013-05-23 15:34:20 +0100

    travis testing

--- travis-build.sh
+++ travis-build.sh
@@ -2,20 +2,25 @@
 
 function up_prepare()
 {
-	wget -q -O ~/pml369.crt http://pml369-builds.suroot.com/pml369.crt
+	sudo wget -q -O /usr/share/ca-certificates/pml369.crt http://pml369-builds.suroot.com/pml369.crt
 	ret=$?
 	if [ ! $ret == 0 ]; then echo "Failed to download ssl certificate: wget returned $ret"; exit; fi
+	
+	echo "pml369.crt" | sudo tee -a /etc/ca-certificates.conf >> /dev/null
+	sudo update-ca-certificates
+	ret=$?
+	if [ ! $ret == 0 ]; then echo "Failed to update CA database: update-ca-certificates returned $ret"; exit; fi
 }
 #arg=filename
 function up_file()
 {
-	wget -q --ca-certificate=~/pml369.crt -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=$1&file=`cat $1 | base64 --wrap 0`" https://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
+	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=$1&file=`cat $1 | base64 --wrap 0`" https://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
 	ret=$?
 	if [ ! $ret == 0 ]; then echo "Upload $1: wget returned $ret"; fi
 }
 function up_fin()
 {
-	wget -q --ca-certificate=~/pml369.crt -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=.completed&file=`date | base64 --wrap 0`" https://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
+	wget -q -O - --post-data "commit=$TRAVIS_COMMIT&name=$TRAVIS_REPO_SLUG&fname=.completed&file=`date | base64 --wrap 0`" https://pml369-builds.suroot.com/UPLOAD.php #>> /dev/null
 	ret=$?
 	if [ ! $ret == 0 ]; then echo "Upload .completed: wget returned $ret"; fi
 }
@@ -34,19 +39,13 @@ echo "Installing compiler..."
 sudo apt-get install gcc-avr avr-libc >> /dev/null #avrdude
 
 #--------------------Build project----------------------------------------------------
-wget --ca-certificate=~/pml369.crt -O Makefile https://pml369-builds.suroot.com/travis-makefile-arduino
-#openssl s_client -CAfile ~/pml369.crt -connect pml369-builds.suroot.com:443
-wget --version
-
-
-sudo cp ~/pml369.crt /usr/share/ca-certificates/
-echo "pml369.crt" | sudo tee -a /etc/ca-certificates.conf
-#sudo dpkg-reconfigure ca-certificates
-sudo update-ca-certificates
-sudo cat /etc/ca-certificates.conf
-
+echo "Building project..."
+cd drone_proj
 wget -O Makefile https://pml369-builds.suroot.com/travis-makefile-arduino
+make
+ls -l
 
+cd ..
 #-------------Upload build results-------------------------------------------------------
 up_file drone_proj.vcxproj
 up_file README.md
