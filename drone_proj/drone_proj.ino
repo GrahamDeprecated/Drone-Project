add622eb4334c3b500ffe3f176c3bcdf32496066
Peter Lotts <plpub2@gmail.com>
2013-05-29 20:56:01 +0100

    tune delay error

--- drone_proj/drone_proj.ino
+++ drone_proj/drone_proj.ino
@@ -133,25 +133,6 @@ digi_serial com(&pins, RF_OUT_BIT_1, RF_IN_BIT_1, RF_IN_INTER);
 	}
 #else
 
-/*// Minimal class to replace std::vector
-template<typename Data>
-class Vector {
-   size_t d_size; // Stores no. of actually stored objects
-   size_t d_capacity; // Stores allocated capacity
-   Data *d_data; // Stores data
-   public:
-     Vector() : d_size(0), d_capacity(0), d_data(0) {}; // Default constructor
-     Vector(Vector const &other) : d_size(other.d_size), d_capacity(other.d_capacity), d_data(0) { d_data = (Data *)malloc(d_capacity*sizeof(Data)); memcpy(d_data, other.d_data, d_size*sizeof(Data)); }; // Copy constuctor
-     ~Vector() { free(d_data); }; // Destructor
-     Vector &operator=(Vector const &other) { free(d_data); d_size = other.d_size; d_capacity = other.d_capacity; d_data = (Data *)malloc(d_capacity*sizeof(Data)); memcpy(d_data, other.d_data, d_size*sizeof(Data)); return *this; }; // Needed for memory management
-     void push_back(Data const &x) { if (d_capacity == d_size) resize(); d_data[d_size++] = x; }; // Adds new value. If needed, allocates more space
-     size_t size() const { return d_size; }; // Size getter
-     Data const &operator[](size_t idx) const { return d_data[idx]; }; // Const getter
-     Data &operator[](size_t idx) { return d_data[idx]; }; // Changeable getter
-   private:
-     void resize() { d_capacity = d_capacity ? d_capacity*2 : 1; Data *newdata = (Data *)malloc(d_capacity*sizeof(Data)); memcpy(newdata, d_data, d_size * sizeof(Data)); free(d_data); d_data = newdata; };// Allocates double the old space
-};*/
-
 	struct note
 	{
 		int freq;
@@ -185,7 +166,7 @@ class Vector {
 		if (_tuneindex < tunes[_tunename].notes.size())
 		{
 			tone(_tunepin,tunes[_tunename].notes[_tuneindex].freq);
-			_tunetimer.after(tunes[_tunename].notes[_tuneindex].time,tune_worker);
+			_tunetimer.after(tunes[_tunename].delay_ms * tunes[_tunename].notes[_tuneindex].time,tune_worker);
 			_tuneindex++;
 		}
 		else
